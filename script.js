b788ebdd859d7333c34faca8a4c0c21faa79aa87
vitalya <vitalay.efimov@gmail.com>
2025-06-19 19:35:34 +0500

    16 Упражнение - Расчёт проекта

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('1');
+const bidSum = 80;
+const clock = 5;
+const wordClock = 40;
+const dey = 11
+
+const sum = (wordClock / clock) * 80
+console.log(`${sum}$`)
+const world = (wordClock / clock) < 11;
+console.log(`Возьмусь за роботу ${world}` )
