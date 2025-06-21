cdb77cb93bcd3277b108d692b6919eb1af867ac2
vitalya <vitalay.efimov@gmail.com>
2025-06-21 17:13:14 +0500

    44 Упражнение - Обновление списка задач

--- script.js
+++ script.js
@@ -33,3 +33,62 @@ const res  = url.split('/');
 console.log(res);
 console.log(roles.join('-'))
 console.log(roles.includes('admin'))
+
+
+// Дан список задач
+// const tasks = ['Задача 1'];
+// Сделать функции:
+// - Добавление задачи в конец
+//   - Удаление задачи по названию
+//     - Перенос задачи в начало списка по названию
+// Всегда меняем исходный массив
+
+
+// const tasks =  ['Задача 1'];
+
+// function addTask(task) {
+//   tasks.push(task)
+//   return tasks
+// }
+// console.log(addTask('Задача 2'));
+
+// function removeTask(task) {
+//   const index = tasks.indexOf(task)
+//   if (index !== -1) {
+//     tasks.splice(index, 1)
+//   }
+//   return tasks
+// }
+// console.log(removeTask('Задача 1'));
+
+
+const tasks = ['Задача 1'];
+
+function Add(task) {
+  tasks.push(task);
+}
+
+function Remove(task) {
+  const index = tasks.indexOf(task); // исправлено index0f -> indexOf
+  if (index !== -1) {
+    tasks.splice(index, 1);
+  }
+}
+
+function Prioritize(task) {
+  const index = tasks.indexOf(task); // исправлено index0f -> indexOf
+  if (index === -1) {
+    return;
+  }
+  const oldTask = tasks[index];
+  tasks.splice(index, 1);
+  tasks.unshift(oldTask);
+}
+
+Add('Задача 2');
+Add('Задача 3');
+console.log(tasks); // ['Задача 1', 'Задача 2', 'Задача 3']
+
+Remove('Задача 1');
+Prioritize('Задача 3');
+console.log(tasks); // ['Задача 3', 'Задача 2']
