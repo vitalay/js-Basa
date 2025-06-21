cf012a7154ef37217433234d9541c9af8d3826f5
vitalya <vitalay.efimov@gmail.com>
2025-06-21 13:36:23 +0500

    38 Упражнение - Кредит на MacBook

--- script.js
+++ script.js
@@ -60,3 +60,31 @@ function getExchengPrais(present1, present2, distance){
   return price1 + price2 + distantsPraci
 }
 console.log(getExchengPrais(1, 5, 20 ));
+
+
+
+/*
+- Возраст
+  - Наличие работы
+    - Деньги
+Нужно проверить может ли он купить новый МасВоо за 2000$ ?
+  Он может брать не только свои деньги, но и взять кредит.
+Ему дадут 500$, только если ему больше 24 - х лет и он имеет работу, 100$ если ему просто больше 24 - х лет и 0 в ином случае.
+Напишите функцию, которая принимает данные пользователя и товара и возвращает true unu false;
+*/
+function computeCredit(age, hasJob = false){
+switch (true) {
+
+  case age > 24 && hasJob:
+    return 500;
+  case age > 24:
+    return 100;
+  default:
+    return 0;
+}
+}
+function canBuy(productPrice, age, money, hasJob = false) {
+const creditMoney = computeCredit(age, hasJob);
+return productPrice <= money + creditMoney;
+}
+console.log(canBuy(2000, 25, 1500,  true));
