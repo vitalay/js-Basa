f48cc6c3ef8613173f9d1018b35d79a6e7c626bb
vitalya <vitalay.efimov@gmail.com>
2025-06-22 15:44:27 +0500

    53 Цикл while

--- script.js
+++ script.js
@@ -45,3 +45,33 @@ for (let i = 0; i < tasks.length; i++) {
     console.log(tasks[i][j]);
   }
 }
+
+// for (let i = 1; i < 5; i++) {
+//   console.log('Вывод - ${i}');
+// }
+//   while
+
+
+const arr = [1, 4, 8, 7];
+
+// for loop with break
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 5) {
+    break;
+  }
+  console.log(arr[i]);
+}
+
+// while loop
+let i = 0;
+while (i < arr.length && arr[i] <= 5) {
+  console.log(arr[i]);
+  i++;
+}
+
+// do...while loop
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 0);
