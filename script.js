2eb687c4bce4e024a5dfb219ba4bc500d8d53c51
vitalya <vitalay.efimov@gmail.com>
2025-06-23 20:25:06 +0500

    61 map

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const raund = [1, 4, 10, 5]
 //   console.log(`raund ${index + 1} ${el}`)
 // }
 
-raund.forEach((el, i) =>{
-  console.log(`Раунд ${i + 1} : ${el}`)
+raund.forEach((raundEl, i) =>{
+  console.log(`Раунд ${i + 1} : ${raundEl}`)
 })
+
+
+const transactionInUSD = [10, -7, 50, -10, 100]
+const transactionInRUB = []
+for (const transaction of transactionInUSD) {
+  transactionInRUB.push(transaction * 60);
+}
+  // console. log(transactionInUSD);
+  // console. Log(transactionInRUB);
+  const transactionInRUB2 = transactionInUSD
+        .map((transaction) => transaction * 60);
+  console.log(transactionInUSD);
+  console.log(transactionInRUB2);
