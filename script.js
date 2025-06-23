a568fc8d62ca6106082f7a7d58b3b503cf088c3f
vitalya <vitalay.efimov@gmail.com>
2025-06-23 19:24:33 +0500

    57 Callback

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
-const operations = [1000, -700, 300, -500, 10000];
-const startingBalance = 100;
-function getBalance(arrayOfOperations, initailBalance) {
-  let balance = initailBalance;
-  for (const element of arrayOfOperations) {
-    balance += element;
-  }
-  return balance
- } 
-  console.log(getBalance(operations, startingBalance));
-
-  function checkOperations(arrayOfOperations, initailBalance) {
-    let balance = initailBalance;
-    let isOk = true;
-    for (const element of arrayOfOperations){
-      balance += element;
-    if (balance < 0){
-    isOk = false;
-    break;
-    }
-    }
-return isOk
-    
+function add(a, b){
+  return a + b;
 }
-console.log(checkOperations(operations, startingBalance));
-
-
-function avarageOperations(arrayOfOperations) {
-  let positiveCount = 0;
-  let positiveSum = 0;
-  let negativeCount = 0
-  let negativeSum = 0;
-  for (const element of arrayOfOperations) {
-    if (element > 0) {
-      positiveCount++;
-      positiveSum += element;
-    }
-      if (element < 0){
-      negativeCount++;
-      negativeSum += element;
-      }
-    }
-      return [positiveSum / positiveCount, negativeSum / negativeCount]
+function sem(a, b) {
+  return a - b;
 }
-console.log(avarageOperations(operations, startingBalance));
+function red(a, b) {
+  return a ** b;
+}
+
+function div(a, b, fn) {
+  const res = fn(a, b);
+  return res
+
+}
+
+console.log(div(10, 2, add,)); // 15
+console.log(div(10, 2, sem)); // 8
+console.log(div(10, 2, red)); // 100
