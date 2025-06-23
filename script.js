e7e66fbe5b947f7ac9292e11931cb2b6ea49867b
vitalya <vitalay.efimov@gmail.com>
2025-06-23 19:31:45 +0500

    58 Возврат функции

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function div(a, b, fn) {
 console.log(div(10, 2, add,)); // 15
 console.log(div(10, 2, sem)); // 8
 console.log(div(10, 2, red)); // 100
+
+function power(pow) {
+  return function (num) {
+    return num ** pow;
+  }
+}
+    const powerOfTwo = power(2);
+    console.log(powerOfTwo(5));
+    console.log(powerOfTwo(10));
+    const powerOfThree = power(3);
+    console.log(powerOfThree(5));
+    console.log(power(5)(4))
