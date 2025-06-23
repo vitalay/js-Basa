fc572be9f75c98ab57bd34372977671b5e802496
vitalya <vitalay.efimov@gmail.com>
2025-06-23 20:13:06 +0500

    60 forEach

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
-function add(a, b){
-  return a + b;
-}
-function sem(a, b) {
-  return a - b;
-}
-function red(a, b) {
-  return a ** b;
-}
+const raund = [1, 4, 10, 5]
 
-function div(a, b, fn) {
-  const res = fn(a, b);
-  return res
-
-}
-
-console.log(div(10, 2, add,)); // 15
-console.log(div(10, 2, sem)); // 8
-console.log(div(10, 2, red)); // 100
-
-// function power(pow) {
-//   return function (num) {
-//     return num ** pow;
-//   }
+// for (const [index, el] of raund.entries()){
+//   console.log(`raund ${index + 1} ${el}`)
 // }
-//     const powerOfTwo = power(2);
-//     console.log(powerOfTwo(5));
-//     console.log(powerOfTwo(10));
-//     const powerOfThree = power(3);
-//     console.log(powerOfThree(5));
-//     console.log(power(5)(4))
 
-
-const  power = (pow) => (num) => num ** pow;
-  
-    const powerOfTwo = power(2);
-    console.log(powerOfTwo(5));
-    console.log(powerOfTwo(10));
-    const powerOfThree = power(3);
-    console.log(powerOfThree(5));
-    console.log(power(5)(4))
+raund.forEach((el, i) =>{
+  console.log(`Раунд ${i + 1} : ${el}`)
+})
