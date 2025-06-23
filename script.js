c122e6faf11cc7e64cd62c68a4d20f105b6df99c
vitalya <vitalay.efimov@gmail.com>
2025-06-23 19:34:58 +0500

    59 Упражнение - Стрелочные функции

--- script.js
+++ script.js
@@ -18,11 +18,21 @@ console.log(div(10, 2, add,)); // 15
 console.log(div(10, 2, sem)); // 8
 console.log(div(10, 2, red)); // 100
 
-function power(pow) {
-  return function (num) {
-    return num ** pow;
-  }
-}
+// function power(pow) {
+//   return function (num) {
+//     return num ** pow;
+//   }
+// }
+//     const powerOfTwo = power(2);
+//     console.log(powerOfTwo(5));
+//     console.log(powerOfTwo(10));
+//     const powerOfThree = power(3);
+//     console.log(powerOfThree(5));
+//     console.log(power(5)(4))
+
+
+const  power = (pow) => (num) => num ** pow;
+  
     const powerOfTwo = power(2);
     console.log(powerOfTwo(5));
     console.log(powerOfTwo(10));
