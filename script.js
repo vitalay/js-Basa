3f5a800c4cfa7608c2c3d6cc865ed1f4bd5b13ba
vitalya <vitalay.efimov@gmail.com>
2025-06-24 19:48:30 +0500

    65 Упражнение - среднее значение

--- script.js
+++ script.js
@@ -61,3 +61,21 @@ const minElement = operations.reduce((acc, operation)  => {
  } ,  0)
 
 console.log(minElement);
+
+/*
+Найти среднее значение последовательность
+числе с помощью reduce
+*/
+const arr = [2, 4, 4, 10];
+const avg = arr.reduce((acc, el, i) => {
+if (i != arr.length - 1) {
+return acc + el;
+} else {
+return (acc + el) / arr.length
+}
+}, 0)
+
+console.log(avg)
+
+const avgq = arr.reduce((acc, el) => acc + el, 0) / arr.length;
+console.log(avgq); // 5
