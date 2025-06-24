64a5f6d0bc525b1ef4574a72960bc2fa2cee7578
vitalya <vitalay.efimov@gmail.com>
2025-06-24 20:15:12 +0500

    68 flat и flatMap

--- script.js
+++ script.js
@@ -99,3 +99,10 @@ console.log(some(arrr, 0))
 
 console.log(arrr.some(el => el === 0))
 
+
+const prices = [[2, 4],[3, 4],[10,[20, 50]]]
+const res1 = prices.flat(2);
+const res2 = prices.flatMap(el => el.concat([1]));
+console.log(res1);
+console.log(res2);
+
