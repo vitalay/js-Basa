3c7add367d1f6f300188167f22743bb9aad3883c
vitalya <vitalay.efimov@gmail.com>
2025-06-25 20:05:41 +0500

    72 Базовые методы.mp4

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-/* вытащить имя и фамилию в отдельные перемнные */
-const fullUserName = 'Вася aka Terminator Пупкин'
-const userName = fullUserName.slice(0, fullUserName.indexOf(' '))
-console.log(userName);
-const userSurname = fullUserName.slice(fullUserName.lastIndexOf(' ') + 1, fullUserName.length)
-console.log(userSurname) ;
+const str = 'Вася Пупкина'
+console.log(str.includes('a'));
+console.log(str.startsWith('B'));
+console.log(str.endsWith('н'));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('B', 'Ф'));
+console.log(str.replace('П', 'Д'));
+console.log(str.replaceAll('a', 'и'));
+console.log(str.replace(/a/g, 'u'));
+const str2 = ' Вася Пупкина \n';
+console.log(str2.trimStart()) 
