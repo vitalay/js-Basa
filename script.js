7ec61f4db8aa8d4bfd0c526f322064eddbf57d36
vitalya <vitalay.efimov@gmail.com>
2025-06-25 20:41:13 +0500

    76 Строки и массивы

--- script.js
+++ script.js
@@ -1,62 +1,8 @@
-// const str = 'Вася Пупкина'
-// console.log(str.includes('a'));
-// console.log(str.startsWith('B'));
-// console.log(str.endsWith('н'));
-// console.log(str.toLowerCase());
-// console.log(str.toUpperCase());
-// console.log(str.replace('B', 'Ф'));
-// console.log(str.replace('П', 'Д'));
-// console.log(str.replaceAll('a', 'и'));
-// console.log(str.replace(/a/g, 'u'));
-// const str2 = ' Вася Пупкина \n';
-// console.log(str2.trimStart()) 
-
-
-
-/* проверить является ли это номером телефона России */
-/* верные */
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7 (910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = ' +7(910) 323-53-56 ';
-/* не верные */
-const num1Error = '89103235';
-const num2Error = '+7d910d323-53-56';
-const num3Error = '9+7103235356';
-const num4Error = '89103g35356';
-function isPhoneNumber(num) {
-  num = num.trim()
-  num = num.replace('+7', '8');
-  if (!num.startsWith('8')) {
-    return false;
-  }
-  num = num.replaceAll('(', '');
-  num = num.replaceAll(')', '');
-  num = num.replaceAll(' ', '');
-  num = num.replaceAll('-', '');
-  num = num.replaceAll('(', '');
-  if (num.length != 11) {
-    return false;
-  }
-  let onlyNumber = true;
-  for (const char of num) {
-    if (isNaN(Number(char))) {
-      onlyNumber = false;
-      break;
-
-    }
-  }
-  return onlyNumber;
-}
-//true
-console.log(isPhoneNumber(num1));
-console.log(isPhoneNumber(num2));
-console.log(isPhoneNumber(num3));
-console.log(isPhoneNumber(num4));
-console.log(isPhoneNumber(num5));
-//false
-console.log(isPhoneNumber(num1Error));
-console.log(isPhoneNumber(num2Error));
-console.log(isPhoneNumber(num3Error));
-console.log(isPhoneNumber(num4Error));
+const form = 'Вася Пупкин Тимофеевичь'
+console.log(form.split());
+const array = [lastNavt, firstName, Name] = form.split(' ');
+console.log(lastNavt);
+console.log(firstName);
+console.log(Name);
+const arr = ['Ты', 'не', 'сказал']
+console.log(arr.join(' '));
