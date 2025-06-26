08d4455cbe6401744f6ef83129a3fcfb34f2d5b6
vitalya <vitalay.efimov@gmail.com>
2025-06-26 20:29:44 +0500

    84 Упражнение - кошелёк

--- script.js
+++ script.js
@@ -9,3 +9,50 @@ const users = {
 }
  
 console.log(users.fullName());
+
+/* Релизовать методы увеличения и уменьшения баланса,
+при котором каждая операция сохраняется в массив
+operations в виде { reason: 'Оплата налогов', sum: -100 }.
+Возвращается true, если успешно и false, если не хватает баланс
+Так же реализовать метод вывода числа операций по кошельку
+*/
+
+
+const wallet = {
+  balance: 0,
+  operations: [],
+
+  increase: function (sum, reason){
+this.balance += sum;
+  this.operations.push({
+    reason: reason,
+    sum: sum
+  })
+     return true;
+},
+    decrease: function (sum, reason){
+if (this.balance < sum) {
+    console.log ('Недостаточно баланса') 
+return false; 
+}
+this.balance -= sum;
+      this.operations.push({
+        reason: reason,
+        sum: -sum
+      })
+return true;
+    },
+  get0perationLenght: function () {
+  return this.operations.length;
+}
+}
+
+
+console.log(wallet.increase(1000, 'Зарплата'));
+console.log(wallet.get0perationLenght());
+console.log(wallet.decrease(2000, 'Покупка ноутбука'));
+console.log(wallet.get0perationLenght());
+console.log(wallet.decrease(500, 'Покупка телефон'));
+console.log(wallet.get0perationLenght());
+console.log(wallet.balance);
+console.log(wallet.operations);
