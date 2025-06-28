831ddec545aae642f2e9b87146874875d3884f5a
vitalya <vitalay.efimov@gmail.com>
2025-06-28 20:34:45 +0500

    96 Пример примитивов и объектов

--- script.js
+++ script.js
@@ -1,114 +1,29 @@
-const users = {
-  name: 'Ivan',
-  sername: 'Efimov',
-  age: 24,
 
- fullName: function () {
-     return this.name + ' ' + this.sername + ' Возраст: ' + this.age
+
+const sum = 1
+console.log(sum);
+function sum2 (n) {
+  n = n + 1
+  if (n > 100) {
+    return n
   }
-}
- 
-console.log(users.fullName());
-
-/* Релизовать методы увеличения и уменьшения баланса,
-при котором каждая операция сохраняется в массив
-operations в виде { reason: 'Оплата налогов', sum: -100 }.
-Возвращается true, если успешно и false, если не хватает баланс
-Так же реализовать метод вывода числа операций по кошельку
-*/
-
-
-const wallet = {
-  balance: 0,
-  operations: [],
-
-  increase: function (sum, reason){
-this.balance += sum;
-  this.operations.push({
-    reason: reason,
-    sum: sum
-  })
-     return true;
-},
-    decrease: function (sum, reason){
-if (this.balance < sum) {
-    console.log ('Недостаточно баланса') 
-return false; 
-}
-this.balance -= sum;
-      this.operations.push({
-        reason: reason,
-        sum: -sum
-      })
-return true;
-    },
-  get0perationLenght: function () {
-  return this.operations.length;
-}
+  console.log(n);
+  return sum2(n)
 }
 
+sum2(sum);
 
-console.log(wallet.increase(1000, 'Зарплата'));
-console.log(wallet.get0perationLenght());
-console.log(wallet.decrease(2000, 'Покупка ноутбука'));
-console.log(wallet.get0perationLenght());
-console.log(wallet.decrease(500, 'Покупка телефон'));
-console.log(wallet.get0perationLenght());
-console.log(wallet.balance);
-console.log(wallet.operations);
+const user = {
+  name: 'Anton',
+  id: 1
 
-
-const cities = {
-  msk: {
-    let: 200,
-    temp: 25
-  },
-  spb: {
-    It: 100,
-    temp: 20
-  }
 }
-let sumTemp = 0;
-let citiesCount = Object.keys(cities).length;
-// for (const key in cities) {
-// sumTemp += cities[key].temp;
-//}
-// console.log(sumTemp / citiesCount);
-for (const key of Object.keys(cities)) {
-  sumTemp += cities[key].temp;
+const user2 = {
+  ...user
 }
-console.log(sumTemp / citiesCount);
+user.name = 'Vasilisa'
+user.id = 2
 
-
-let user = {
-  name: 'Вася',
-  age: 40,
-  city: 'Moscow'
-}
-const { age, ...userWithouAge } = user;
-console.log(age);
-console.log(userWithouAge);
-
-const additionalData = {
-  skills: ['Разработка', 'Дизайн'],
-  creditCard: '2342-2345-2734-2356'
-};
-  user = {
-...user,
-...additionalData
-  }
 console.log(user);
-
-const cities1 = {
-  msk: {
-    temp: {
-      celcius: 25
-    }
-  },
-spb: {
-  }
-}
-const city = 'msk'
-if (cities1[city] != undefined && cities1[city].temp != undefined)
-  console.log(cities1[city].temp.celcius);
-console.log(cities1[city]?.temp?.celcius);
+console.log(user2);
+ 
