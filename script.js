8284e949350a87bcbc61a63a69211326a492af41
vitalya <vitalay.efimov@gmail.com>
2025-06-29 22:05:25 +0500

    110 Упражнение - управление this

--- script.js
+++ script.js
@@ -48,3 +48,46 @@ addDamageFunc.call(bmw, 'Бампер', 2);
 addDamageFunc.call(audi, 'Бампер', 2);
 addDamageFunc.apply(bmw, ['Бампер', 2]);
 addDamageFunc.apply(audi, ['Бампер', 2])
+
+
+
+const car = {
+  marka: 'kalina',
+  madele: 'x5',
+  damadge: []
+}
+const addCarManipulation = {
+ addCar(rag, par){
+    car.damadge.push({
+      rag,
+      par
+      })
+    console.log(`Повреждения ${this.marka} ${this.madele}`);
+}
+}
+const aaaddd = addCarManipulation.addCar.bind(car)
+aaaddd('Крыло', 3)
+const aaaddd2 = addCarManipulation.addCar.bind(car, 'Kрыша')
+aaaddd2(5)
+aaaddd2(3)
+console.log(car);
+
+// */
+//   Создайте объект пользователя с паролем.
+// С помощью функции ниже удалить пароль сделав
+// функцию сброса пароля
+//   */
+function removePassword(reset) {
+  if (reset) {
+    this.password = undefined;
+  } else {
+    this.password = '1';
+  }
+}
+const user = {
+  login: 'a@a.ru',
+  password: '12345'
+} ;
+  const resetUserPassword = removePassword.bind(user, true);
+  resetUserPassword() ;
+console.log(user)
