84c9ec6222f5f467546a89a3a10217230411c420
vitalya <vitalay.efimov@gmail.com>
2025-06-29 17:23:27 +0500

    107 EOL для методов

--- script.js
+++ script.js
@@ -1,65 +1,16 @@
 
 'use strict';
 
-// const user = {
-//   firstName: 'Vasy',
-//   lastName: 'Vasilievich',
-//   age: 15,
-//   addUserInfo: function () {
-//     console.log(`${this.firstName}  ${this.lastName}`);
+const b = 1
 
-//     const canDrink = () => {
-//       if (this.age >= 18) {
-//         console.log('Может Пить');
-//       } else {
-//         console.log('Не может пить ');
-//       }
-
-//     }
-//     canDrink()
-//   },
-
-//   addUserInfoArro: () => {
-//     console.log(`${this.firstName}  ${this.lastName}`);
-//   }
-// }
-
-// user.addUserInfo()
-// user.addUserInfoArro()
-
-
-/*
-дополнить объект методами для получения имени:
-- компании
-- ceo
-- сотрудника
-*/
-const company = {
-  name: '000 Arpo',
-    // addName: function (){
-    //   console.log(`Компания: ${this.name}`)
-    // },
-  employees: [
-    {
-      
-      name: 'Света',
-        getName: function () {
-        return this.name
-      }
-    }
-  ],
-
-  ceo: {
-    name: 'Вася',
-      getName: function () {
-      return this.name
-    }
-
-  },
-  getName: function (){
-    return this.name
-  }
-} 
-console.log(company.getName()); 
-console.log(company.ceo.getName()); 
-console.log(company.employees.map(employees => employees.getName()));
+const a = {
+   b,
+   getB: function(){
+    return this.b
+   },
+   getBAlt(){
+    return this.b
+   }
+}
+console.log(a.getB());
+console.log(a.getBAlt());
