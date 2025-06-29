634195d6c944d6fa808871e14c9a6c9fb6866b91
vitalya <vitalay.efimov@gmail.com>
2025-06-29 17:49:22 +0500

    108 Call, apply

--- script.js
+++ script.js
@@ -4,13 +4,47 @@
 const b = 1
 
 const a = {
-   b,
-   getB: function(){
+  b,
+  getB: function () {
     return this.b
-   },
-   getBAlt(){
+  },
+  getBAlt() {
     return this.b
-   }
+  }
 }
 console.log(a.getB());
 console.log(a.getBAlt());
+
+
+// ____________________________________________________________________
+const audi = {
+  make: 'Audi',
+  model: 'АЗ',
+  year: 2021,
+  damages: [],
+  addDamage(part, rate) {
+    console.log(`у Авто: ${this.make} ${this.model} ${this.year}  добавлено: - повреждение ${part} со степенью ${rate}`);
+    this.damages.push({
+      part,
+      rate
+    })
+  },
+
+}
+audi.addDamage('капот', 3)
+
+const bmw = {
+  make: 'BMW',
+  model: ' X5',
+  year: 2022,
+  damages: [],
+}
+bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2)
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc ('Бампер', 2);
+addDamageFunc.call(bmw, 'Бампер', 2);
+addDamageFunc.call(audi, 'Бампер', 2);
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+addDamageFunc.apply(audi, ['Бампер', 2])
