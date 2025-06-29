bf65a1592f281ac8f53354aac39a28987c04290b
vitalya <vitalay.efimov@gmail.com>
2025-06-29 17:15:22 +0500

    106 Упражнение - объект в объекте

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 
 'use strict';
 
-const user = {
-  firstName: 'Vasy',
-  lastName: 'Vasilievich',
-  age: 15,
-  addUserInfo: function () {
-    console.log(`${this.firstName}  ${this.lastName}`);
+// const user = {
+//   firstName: 'Vasy',
+//   lastName: 'Vasilievich',
+//   age: 15,
+//   addUserInfo: function () {
+//     console.log(`${this.firstName}  ${this.lastName}`);
 
-    const canDrink = () => {
-      if (this.age >=  18) {
-        console.log('Может Пить');
-      } else {
-        console.log('Не может пить ');
+//     const canDrink = () => {
+//       if (this.age >= 18) {
+//         console.log('Может Пить');
+//       } else {
+//         console.log('Не может пить ');
+//       }
+
+//     }
+//     canDrink()
+//   },
+
+//   addUserInfoArro: () => {
+//     console.log(`${this.firstName}  ${this.lastName}`);
+//   }
+// }
+
+// user.addUserInfo()
+// user.addUserInfoArro()
+
+
+/*
+дополнить объект методами для получения имени:
+- компании
+- ceo
+- сотрудника
+*/
+const company = {
+  name: '000 Arpo',
+    // addName: function (){
+    //   console.log(`Компания: ${this.name}`)
+    // },
+  employees: [
+    {
+      
+      name: 'Света',
+        getName: function () {
+        return this.name
       }
- 
-    } 
-    canDrink()
+    }
+  ],
+
+  ceo: {
+    name: 'Вася',
+      getName: function () {
+      return this.name
+    }
+
   },
-
-  addUserInfoArro: () => {
-    console.log(`${this.firstName}  ${this.lastName}`);
+  getName: function (){
+    return this.name
   }
-}
-
-user.addUserInfo()
-user.addUserInfoArro() 
+} 
+console.log(company.getName()); 
+console.log(company.ceo.getName()); 
+console.log(company.employees.map(employees => employees.getName()));
